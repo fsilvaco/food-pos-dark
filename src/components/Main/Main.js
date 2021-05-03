6c9a359d1f5df47656ebc97da16185a97f09ebdb
Flavio Silva <contatoflaviodev@gmail.com>
2021-05-03 16:31:51 -0300

    Criando componente Main e subindo API

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,35 @@
+import { useEffect, useState } from "react"
 import styled from "styled-components"
 import { Heading } from "../Navigation/Heading"
 import { Menu } from "../Navigation/Menu"
 
+import api from "../../services/api"
+
 export function Main() {
+    const [food, setFood] = useState()
+
+    async function handleFoods() {
+        const {data} = await api.get("/foods")
+        setFood(data)
+        }
+
+    useEffect(() =>{
+        handleFoods()
+    }, [])
+
     return (
         <Container>
             <Heading />
             <Menu />
+
+            <CardContainer> 
+                <CardTitle>Choose Dishes</CardTitle>
+
+                <AllCardContainer>
+                    {/* <Card item={}  /> */}
+                </AllCardContainer>
+
+            </CardContainer>
         </Container>
     )
 }
@@ -16,4 +39,13 @@ const Container = styled.div`
     flex-direction: column;
     width: 55%;
     padding: 25px;
+`
+
+const CardContainer = styled.div`
+`
+
+const CardTitle = styled.h2`
+`
+
+const AllCardContainer = styled.div`
 `
